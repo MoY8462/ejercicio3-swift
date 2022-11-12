a2adefcd62d42e59952f19978fd4f8c896a5c865
Moisés Sánchez <moy.sav21@hotmail.com>
2022-11-12 12:53:29 -0600

    style: :art: add new fonts style

--- Ejercicio3/ViewController.swift
+++ Ejercicio3/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var btnTwitter: UIButton!
+    @IBOutlet var btnInstagram: UIButton!
+    @IBOutlet var btnFacebook: UIButton!
+    @IBOutlet var btnYoutube: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func twitter(_ sender: UIButton) {
+    }
+    
+    @IBAction func facebook(_ sender: UIButton) {
+    }
+    
+    @IBAction func youtube(_ sender: UIButton) {
+    }
+    
+    @IBAction func instagram(_ sender: UIButton) {
+    }
 }
 
